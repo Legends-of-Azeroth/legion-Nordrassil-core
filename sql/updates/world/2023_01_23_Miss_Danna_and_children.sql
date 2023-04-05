eb435c44c570601f5917a5d771bcdc9abf3d1f02
Terragor <81566364+zTerragor@users.noreply.github.com>
2023-04-05 22:20:19 +0500

    +

--- sql/updates/world/2023_01_23_Miss_Danna_and_children.sql
+++ sql/updates/world/2023_01_23_Miss_Danna_and_children.sql
@@ -23,6 +23,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (3513, 0, 4, 0, 61, 0, 100, 0, 0, 0, 0, 0, 54, 18000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Link - Pause WP'),
 (3513, 0, 5, 6, 40, 0, 100, 0, 50, 0, 0, 0, 80, 351301, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'WP Reached - Actionlist for Conversation @ Valley'),
 (3513, 0, 6, 0, 61, 0, 100, 0, 0, 0, 0, 0, 54, 20000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Link - Pause WP'),
+(3513, 0, 7, 0, 40, 0, 100, 0, 113, 0, 0, 0, 1, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'WP Reached - Speech before Cathedral'),
 (3513, 0, 8, 9, 40, 0, 100, 0, 125, 0, 0, 0, 80, 351302, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'WP Reached - Actionlist for Conversation @ Cathedral'),
 (3513, 0, 9, 0, 61, 0, 100, 0, 0, 0, 0, 0, 54, 22000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Link - Pause WP'),
 (3513, 0, 100, 0, 22, 0, 100, 0, 101, 2000, 2000, 0, 5, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Receive Wave Emote - Wave'),
@@ -58,12 +59,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (351300, 9, 2, 0, 0, 0, 100, 0, 2000, 2000, 0, 0, 84, 0, 0, 0, 0, 0, 0, 11, 3511, 15, 0, 0, 0, 0, 0, 'Simple Talk - Steven'),
 (351300, 9, 3, 0, 0, 0, 100, 0, 6000, 6000, 0, 0, 1, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Speech');
 
-DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` = 351301);
+DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` = 351300);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(351301, 9, 0, 0, 0, 0, 100, 0, 2000, 2000, 0, 0, 1, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Speech'),
-(351301, 9, 1, 0, 0, 0, 100, 0, 8000, 8000, 0, 0, 1, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Speech'),
-(351301, 9, 2, 0, 0, 0, 100, 0, 2000, 2000, 0, 0, 84, 3, 0, 0, 0, 0, 0, 11, 3511, 15, 0, 0, 0, 0, 0, 'Simple Talk - Steven'),
-(351301, 9, 3, 0, 0, 0, 100, 0, 6000, 6000, 0, 0, 1, 5, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Speech');
+(351300, 9, 0, 0, 0, 0, 100, 0, 2000, 2000, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Speech'),
+(351300, 9, 1, 0, 0, 0, 100, 0, 8000, 8000, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Speech'),
+(351300, 9, 2, 0, 0, 0, 100, 0, 2000, 2000, 0, 0, 84, 0, 0, 0, 0, 0, 0, 11, 3511, 15, 0, 0, 0, 0, 0, 'Simple Talk - Steven'),
+(351300, 9, 3, 0, 0, 0, 100, 0, 6000, 6000, 0, 0, 1, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Speech');
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` = 351302);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
